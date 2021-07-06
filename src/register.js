0820ee95a7d1cc4d328d6a0c94ad7cb32a6801b7
Prativa01 <prativastha876@gmail.com>
2021-07-06 12:19:40 +0545

    first commit

---
+++ src/register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './register.css';
+
+class register extends Component{
+    render(){
+    return(
+    <div className="Register">
+        
+    </div>
+    )
+    }
+}
+export default register;

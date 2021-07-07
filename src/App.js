143e76d6b623b36f9007efb0d0cf77a98976996b
Prativa01 <prativastha876@gmail.com>
2021-07-07 11:03:27 +0545

    header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Switch, Route}
 from "react-router-dom";
 import Register from "./register"
 import './App.css';
+import Header from "./Header";
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
       <div className="App">
         <Switch>
           <Route path ="/register">
+             <Header/>
             <Register/>
           </Route>
       </Switch>

107a48d0345c0ed72d5e986408bae9da23e0398d
Reshika Shrestha <shresthareshika@gmail.com>
2021-07-22 15:24:24 +0545

    design of contact page

--- src/Contact.js
+++ src/Contact.js
@@ -1,36 +1,37 @@
 import React from 'react';
-import './Contact.css'
+import './Contact.css';
+import { FaLocationArrow,FaGoogleDrive,FaPhoneAlt } from 'react-icons/fa';
 
 function Contact() {
     return (
         
     <section id="contact" class="contact">
       <div class="container">
-
-        {/* <div>
-          <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
-        </div> */}
+   
+     
 
         <div class="row mt-5">
-
+        
           <div class="col-lg-4">
             <div class="info">
+            <h2>Contact Us</h2>
               <div class="address">
-                <i class="bi bi-geo-alt"></i>
+              <i class="bi bi-geo-alt"><FaLocationArrow/></i>
                 <h4>Location:</h4>
-                <p>A108 Adam Street, New York, NY 535022</p>
+                <p>Dillibazar <br/>
+                Kathmandu, nepal</p>
               </div>
 
               <div class="email">
-                <i class="bi bi-envelope"></i>
+              <i class="bi bi-envelope"><FaGoogleDrive/></i>
                 <h4>Email:</h4>
-                <p>info@example.com</p>
+                <p>ridesharing@email.com</p>
               </div>
 
               <div class="phone">
-                <i class="bi bi-phone"></i>
+                <i class="bi bi-phone"><FaPhoneAlt/></i>
                 <h4>Call:</h4>
-                <p>+1 5589 55488 55s</p>
+                <p>+977 987654321</p>
               </div>
 
             </div>
@@ -40,7 +41,9 @@ function Contact() {
           <div class="col-lg-8 mt-5 mt-lg-0">
 
             <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+            <h2>Feel free to ask anything</h2>
               <div class="row">
+             
                 <div class="col-md-6 form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required/>
                 </div>
@@ -48,12 +51,13 @@ function Contact() {
                   <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required/>
                 </div>
               </div>
-              <div class="form-group mt-3">
+              <div class="col-md-6 form-group mt-3">
                 <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required/>
               </div>
-              <div class="form-group mt-3">
+              <div class="col-md-6 form-group mt-3">
                 <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
               </div>
+              
               <div class="my-3">
                 <div class="loading">Loading</div>
                 <div class="error-message"></div>

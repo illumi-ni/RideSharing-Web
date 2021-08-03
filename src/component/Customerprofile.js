0fb579a9de40de53f860c889cd550cbfcdfc81f3
roy94576 <roy94576@gmail.com>
2021-08-03 15:25:15 +0545

    some changes in customer profile design

--- src/component/Customerprofile.js
+++ src/component/Customerprofile.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/Customerprofile.css';
 import { Link } from 'react-router-dom';
+import profile from "../images/profile.jpg"
 
 class Customerprofile extends Component{
     render(){
@@ -32,28 +33,32 @@ class Customerprofile extends Component{
                             </Link>
                         </ul>
                     </div>
-                    <form>
-            <div class="form-group">
-                <label for="Fullname">Full Name</label>
-                <input type="Fullname" class="form-control" id="FullName" placeholder="Enter fullName"/>
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="Enter Email"/>
-            </div>
-            <div class="form-group">
-                <label for="Phone">Phone No</label>
-                <input type="Phone" class="form-control" id="Phone" placeholder="Enter Phone No"/>
-            </div>
-            <div class="form-group">
-                <label for="gender">Gender</label>
-                <input type="gender" class="form-control" id="gender" placeholder="Enter Gender"/>
-             </div>
-            <div class="form-group">
-                <label for="gender">Date of Birth</label>
-                <input type="dateofbirth" class="form-control" id="dateofbirth" placeholder="Enter Date of Birth"/>
-            </div>
-            </form>
+                    <form className="form1">
+                    <img src={profile} class="card-img-top" alt="" style={{width:"200px"}}></img>
+                    <button type="button" class="btn btn-primary">Upload Picture</button>
+                    <div className="form2">
+                        <div class="form-group">
+                            <label for="Fullname">Full Name</label>
+                            <input type="Fullname" class="form-control" id="FullName" placeholder="Enter fullName"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="email">Email</label>
+                            <input type="email" class="form-control" id="email" placeholder="Enter Email"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="Phone">Phone No</label>
+                            <input type="Phone" class="form-control" id="Phone" placeholder="Enter Phone No"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="gender">Gender</label>
+                            <input type="gender" class="form-control" id="gender" placeholder="Enter Gender"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="gender">Date of Birth</label>
+                            <input type="dateofbirth" class="form-control" id="dateofbirth" placeholder="Enter Date of Birth"/>
+                        </div>
+                        </div>
+                        </form>
 
                 </div>
                  </section>
